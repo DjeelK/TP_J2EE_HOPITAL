772a974d0ac3830a2b3ced93d6fe7514bbd69d05
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-06-06 16:42:59 +0200

    affichage liste patients ok

--- src/main/java/com/example/tp_hopital/PatientServlet.java
+++ src/main/java/com/example/tp_hopital/PatientServlet.java
@@ -1,59 +1,49 @@
 package com.example.tp_hopital;
-
-import entities.Patient;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import services.PatientService;
-
+import util.Definition;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 @WebServlet(name="patients", value ="/patients")
 public class PatientServlet extends HttpServlet {
     private PatientService service;
-    public void init(){
+
+    public void init() {
         service = new PatientService();
     }
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        if(request.getParameter("nom_patient") != null){
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if (request.getParameter("nom_patient") != null) {
             String nom_patient = request.getParameter("nom_patient");
             String prenom_patient = request.getParameter("prenom_patient");
             String mail_patient = request.getParameter("mail_patient");
             String tel_patient = request.getParameter("tel_patient");
-            Patient patient = new Patient(nom_patient,prenom_patient,mail_patient,tel_patient);
-            if(service.create(patient)) {
-                out.println("<h1> Patient ajouté");
+            entities.Patient patient = new entities.Patient(nom_patient, prenom_patient, mail_patient, tel_patient);
+            if (service.create(patient)) {
+                response.sendRedirect("patients");
             }
         } else {
-            out.println("<h1> Erreur lors de l'ajout du patient</h1>");
         }
-        out.println("</body></html>");
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        if(request.getParameter("id") != null) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if (request.getParameter("id") != null) {
             int id = Integer.parseInt((request.getParameter("id")));
-            Patient patient = service.findById(id);
-            out.println("<h1>Patient </h1>");
-            out.println("<div>"+patient+ "</div>");
+            entities.Patient patient = service.findById(id);
+            request.setAttribute("patient", patient);
+            request.getRequestDispatcher(Definition.VIEW_PATH + "patient.jsp").forward(request, response);
+        } else {
+            request.setAttribute("patients", service.findAll());
+            request.getRequestDispatcher(Definition.VIEW_PATH + "patients.jsp").forward(request, response);
         }
-        else {
-            out.println("<h1>Liste des patients</h1>");
-            for(Patient p : service.findAll()) {
-                out.println("<div>"+p+ "</div>");
-            }
-        }
-        out.println("</body></html>");
+
     }
 }
+
 
 
